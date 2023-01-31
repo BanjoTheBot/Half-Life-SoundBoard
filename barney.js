82e5da07b9f0018b405ee7b1249052b46a91ae20
Lachlan Paul <lachlanpaul03@gmail.com>
2023-01-31 17:24:27 +1030

    Added Barney Voice Lines and Tab

---
+++ barney.js
@@ -0,0 +1,35 @@
+// function (){
+// var snd = new Audio("./sound/barney/.wav");
+//     snd.play();
+//     snd.currentTime=0;
+// }
+
+function dontmake(){
+    var snd = new Audio("./sound/barney/ba_dontmake.wav");
+    snd.play();
+    snd.currentTime=0;
+}
+
+function aintgoin(){
+    var snd = new Audio("./sound/barney/aintgoin.wav");
+        snd.play();
+        snd.currentTime=0;
+}
+
+function hevyes(){
+    var snd = new Audio("./sound/barney/c1a0_ba_hevyes.wav");
+        snd.play();
+        snd.currentTime=0;
+}
+
+function beer(){
+    var snd = new Audio ("./sound/barney/ba_later.wav")
+        snd.play();
+        snd.currentTime=0;
+}
+
+function friends(){
+var snd = new Audio("./sound/barney/ba_friends.wav");
+    snd.play();
+    snd.currentTime=0;
+}
